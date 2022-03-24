650a24c6e8293238ceaa9549a84ef342f8bce008
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2022-03-25 00:08:23 +0900

    쓰레드 로컬을 활용한 로그 추적기 개발

--- spring/inflearn/spring-advanced/advanced/src/main/java/hello/advanced/LogTraceConfig.java
+++ spring/inflearn/spring-advanced/advanced/src/main/java/hello/advanced/LogTraceConfig.java
@@ -1,15 +1,16 @@
 package hello.advanced;
 
-import hello.advanced.trace.logtrace.FieldLogTrace;
 import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LogTraceConfig {
 
+    // 크으... 예술이다...
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
