3e2de1af8d5a3e9d9a893932fc16fc38ad706131
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2022-03-24 23:25:48 +0900

    ThreadLocal 예제 코드 작성

---
+++ spring/inflearn/spring-advanced/advanced/src/test/java/hello/advanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,43 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+
+    private ThreadLocalService fieldService = new ThreadLocalService();
+
+    @Test
+    void field() {
+        log.info("main start");
+        Runnable userA = () -> {
+            fieldService.logic("userA");
+        };
+
+        Runnable userB = () -> {
+            fieldService.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+//        sleep(2000);// 동시성 문제가 발생하지 않는 경우
+        sleep(100);// 동시성 문제가 발생하는 경우
+        threadB.start();
+        sleep(3000);
+        log.info("main exit");
+    }
+
+    private void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
