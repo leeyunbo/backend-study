015e3b1a1b34b9fce5794d3979cebb0a0804d4f9
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2022-03-30 21:29:20 +0900

    탬플릿 메서드 패턴을 활용한 V5 로그 추적기 개발

---
+++ spring/inflearn/spring-advanced/advanced/src/main/java/hello/advanced/app/v5/OrderControllerV5.java
@@ -0,0 +1,29 @@
+package hello.advanced.app.v5;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import hello.advanced.trace.template.AbstractTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV5 {
+
+    private final OrderServiceV5 orderService;
+    private final LogTrace trace;
+
+    @GetMapping("/v5/request")
+    public String request(String itemId) {
+        AbstractTemplate<String> template = new AbstractTemplate(trace) {
+            @Override
+            protected String call() {
+                orderService.orderItem(itemId);
+                return "ok";
+            }
+        };
+
+        return template.execute("OrderController.request()");
+    }
+}
