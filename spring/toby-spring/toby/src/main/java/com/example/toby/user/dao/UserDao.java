1450bbc4efb5390853e1511d8188c932eb9bbc0d
Lee yun bok <44944031+leeyunbo@users.noreply.github.com>
2022-03-30 22:44:07 +0900

    toby spring project init (유저 정보 저장, 가져오기)

---
+++ spring/toby-spring/toby/src/main/java/com/example/toby/user/dao/UserDao.java
@@ -0,0 +1,68 @@
+package com.example.toby.user.dao;
+
+import com.example.toby.user.domain.User;
+import org.springframework.stereotype.Component;
+
+import java.sql.*;
+
+@Component
+public class UserDao {
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Class.forName("org.mariadb.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mariadb://localhost/toby", "root", "dbsqhr1!62719");
+
+        PreparedStatement ps = c.prepareStatement(
+                "insert into users(id, name, passowrd) values(?,?,?)");
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        c.close();
+    }
+
+    public User get(String id) throws ClassNotFoundException, SQLException {
+        Class.forName("org.mariadb.jdbc.Driver");
+        Connection c = DriverManager.getConnection(
+                "jdbc:mariadb://localhost/toby", "root", "dbsqhr1!62719");
+
+        PreparedStatement ps = c.prepareStatement(
+                "select * from users where id = ?");
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        User user = new User();
+        user.setId(rs.getString("id"));
+        user.setName(rs.getString("name"));
+        user.setPassword(rs.getString("passowrd"));
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return user;
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException{
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("fffff");
+        user.setName("이윤복");
+        user.setPassword("pass");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공");
+    }
+}
---
+++ spring/toby-spring/toby/src/main/java/com/example/toby/user/domain/User.java
@@ -0,0 +1,12 @@
+package com.example.toby.user.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    private String id;
+    private String name;
+    private String password;
+}
